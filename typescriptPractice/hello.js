8dbe2abce2b75586726a33c207366f99d31b3c24
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-10-22 02:35:01 +0900

    typescript 공부 1

---
+++ typescriptPractice/hello.js
@@ -0,0 +1,5 @@
+function greet(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+}
+greet('sungSSbin', new Date());
+var msg = 'string!!';
---
+++ typescriptPractice/hello.ts
@@ -0,0 +1,7 @@
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet('sungSSbin', new Date());
+
+let msg = 'string!!';
