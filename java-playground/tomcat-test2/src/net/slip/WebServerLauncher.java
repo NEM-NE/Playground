d744d228d7355ade9fc2ec6d706a685d88d798e0
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-25 13:15:02 +0900

    ..

---
+++ java-playground/tomcat-test2/src/net/slip/WebServerLauncher.java
@@ -0,0 +1,31 @@
+package net.slipp;
+
+import java.io.File;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class WebServerLauncher {
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        String webPort = System.getenv("PORT");
+
+        if(webPort == null || webPort.isEmpty())
+        {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(webPort));
+        Connector connector = tomcat.getConnector();
+        connector.setURIEncoding("UTF-8");
+        
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println(new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+    }
+}
