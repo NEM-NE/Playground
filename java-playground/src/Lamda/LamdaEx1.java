b46942ee864c9e09b0e4d1df78020571b011b4e3
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-04-12 14:02:25 +0900

    add: 람다 스트림 이넘

--- java-playground/src/Lamda/LamdaEx1.java
+++ java-playground/src/Lamda/LamdaEx1.java
@@ -1,7 +1,8 @@
 package Lamda;
 
 import java.util.Arrays;
-import java.util.function.IntUnaryOperator;
+import java.util.Comparator;
+import java.util.function.*;
 
 public class LamdaEx1 {
     public static void main(String[] args) {
@@ -14,5 +15,44 @@ public class LamdaEx1 {
                 return (int)(Math.random() * 5) + 1;
             }
         });
+
+        Function<Integer, Integer> func = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer) {
+                return integer;
+            }
+        };
+
+        Function<Integer, Integer> func2 = (o1) -> o1;
+
+        Consumer<Integer> consumer = System.out::println;
+
+        Function<Integer, Integer> andThen = func.andThen(func2);
+
+        Predicate<String> predicate = (String str) -> str.startsWith("T");
+
+        Supplier<String> supplier = () -> "hihi";
+
+        func.apply(4);
+        func.apply(3);
+
+        consumer.accept(1515);
+
+        boolean isTAXI = predicate.test("BUS");
+        System.out.println(isTAXI);
+
+        supplier.get();
+
+        Function<MMethod, String> methodFunc = MMethod::sayHello;
+    }
+}
+
+class MMethod {
+    public MMethod(){
+
+    }
+
+    public String sayHello() {
+        return "hihi";
     }
 }
